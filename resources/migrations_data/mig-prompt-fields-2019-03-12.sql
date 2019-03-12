0e493d9fb0bf1cc33c85019e6c791524ed22e15c
Bill Piel <bill@vetd.com>
2019-03-12 15:39:21 +0000

    add price estimate details textbox

---
+++ resources/migrations_data/mig-prompt-fields-2019-03-12.sql
@@ -0,0 +1,2 @@
+COPY vetd.prompt_fields (deleted, fname, ftype, fsubtype, updated, created, prompt_id, idstr, id, list_qm, descr, sort) FROM stdin;
+\N	details	s	multi	2019-02-12 15:50:25.135-05	2019-02-12 15:50:25.135-05	370382503629	h2n7sgwk	610341390082	f	\N	2
