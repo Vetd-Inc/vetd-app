5c88a95636ca19ec80238245d75b4a47d3b23605
Christopher Elwell <elwell.christopher@gmail.com>
2019-03-13 11:47:23 -0400

    oops, need this ns stub for now

---
+++ src/cljs/app/vetd_app/analytics.cljs
@@ -0,0 +1,18 @@
+(ns vetd-app.analytics
+  (:require [re-frame.core :as rf]))
+
+;; (rf/reg-fx
+;;  :analytics/identify
+;;  )
+
+;; (rf/reg-fx
+;;  :analytics/track
+;;  )
+
+;; (rf/reg-fx
+;;  :analytics/page
+;;  )
+
+;; (rf/reg-fx
+;;  :analytics/group
+;;  )
