6d0ba0d57a92ec4c2a0e238cfed44dece5ea234e
Bill Piel <bill@billpiel.com>
2019-04-01 18:54:06 +0000

    starting vendor profile migration

--- dev/clj/tasks.clj
+++ dev/clj/tasks.clj
@@ -1,30 +1,70 @@
 (ns tasks
-  (:require [com.vetd.app.db :as db]))
+  (:require [com.vetd.app.db :as db]
+            [com.vetd.app.hasura :as ha] ))
 
 ;; - count vendors/products without profiles
 ;; - limited select vendors/products without profiles
 ;; - find profile form
 ;; - create doc&response from profile
 
-(db/hs-query
- {:select [:%count.*]
-  :from [[:orgs :o]]
-  :left-join [[:docs :d]
-              [:and
-               [:= :d.from_org_id :o.id]
-               [:= :d.dtype "vendor-profile"]
-               [:= :d.deleted nil]]]
-  :where [:and
-          [:= :o.vendor_qm true]
-          [:= :d.from_org_id nil]
-          [:= :o.deleted nil]]})
 
+(defn count-vendors-without-profile []
+  (db/hs-query
+   {:select [:%count.*]
+    :from [[:orgs :o]]
+    :left-join [[:docs :d]
+                [:and
+                 [:= :d.from_org_id :o.id]
+                 [:= :d.dtype "vendor-profile"]
+                 [:= :d.deleted nil]]]
+    :where [:and
+            [:= :o.vendor_qm true]
+            [:= :d.id nil]
+            [:= :o.deleted nil]]}))
 
-(db/hs-query
- {:select [:*]
-  :from [[:forms :f]]
-  :where [:and
-          [:= :f.ftype "vendor-profile"]
-          [:= :f.deleted nil]]
-  :order-by {:f.created :desc}
-  :limit 1})
+(defn get-vendors-without-profile-with-limit [limit]
+  (db/hs-query
+   {:select [:o.*]
+    :from [[:orgs :o]]
+    :left-join [[:docs :d]
+                [:and
+                 [:= :d.from_org_id :o.id]
+                 [:= :d.dtype "vendor-profile"]
+                 [:= :d.deleted nil]]]
+    :where [:and
+            [:= :o.vendor_qm true]
+            [:= :d.id nil]
+            [:= :o.deleted nil]]
+    :limit limit}))
+
+(defn get-latest-vendor-profile-form []
+  (db/hs-query
+   {:select [:*]
+    :from [[:forms :f]]
+    :where [:and
+            [:= :f.ftype "vendor-profile"]
+            [:= :f.deleted nil]]
+    :order-by {:f.created :desc}
+    :limit 1}))
+
+(defn get-latest-vendor-profile-form []
+  (ha/sync-query [[:forms
+                   {:ftype "vendor-profile"
+                    :deleted nil
+                    :_limit 1}
+                   [:id
+                    :title
+                    :ftype
+                    :fsubtype
+                    [:prompts {:deleted nil}
+                     [:id 
+                      :prompt
+                      :sort
+                      [:fields {:deleted nil}
+                       [:id 
+                        :fname
+                        :ftype
+                        :fsubtype
+                        :list?
+                        :sort]]]]]]]))
+
