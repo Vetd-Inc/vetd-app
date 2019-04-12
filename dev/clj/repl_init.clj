53a6f83d5fd19cd8937dfe56601f426e044a150d
Christopher Elwell <elwell.christopher@gmail.com>
2019-04-12 15:21:21 +0000

    dont try to import logger

--- dev/clj/repl_init.clj
+++ dev/clj/repl_init.clj
@@ -2,12 +2,7 @@
   (:require [com.vetd.app.core :as core]
             [figwheel-sidecar.repl-api :as fw]))
 
-#_(do
-    (import '(org.apache.log4j Logger Level))
-
-    (.setLevel ( org.slf4j.LoggerFactory/getLogger org.slf4j.Logger/ROOT_LOGGER_NAME)
-               ch.qos.logback.classic.Level/INFO))
-
-
+#_(.setLevel ( org.slf4j.LoggerFactory/getLogger org.slf4j.Logger/ROOT_LOGGER_NAME)
+             ch.qos.logback.classic.Level/INFO)
 
 (core/-main)
