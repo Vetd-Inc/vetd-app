d8f3160b89136479532fb505dd1c33a294c55b41
Bill Piel <bill@billpiel.com>
2019-09-04 19:04:09 +0000

    auto populate categories on product form on page load

--- src/cljs/app/vetd_app/core.cljs
+++ src/cljs/app/vetd_app/core.cljs
@@ -8,6 +8,7 @@
             vetd-app.debounce
             vetd-app.common.fx
             vetd-app.orgs.fx
+            [vetd-app.util :as util]
             [vetd-app.hooks :as hooks]
             [vetd-app.buyers.fixtures :as b-fix]
             [vetd-app.buyers.pages.search :as p-bsearch]
