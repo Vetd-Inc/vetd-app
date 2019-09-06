9039e53feadb4ad7dd717afdad70e21f43d18cd5
Christopher Elwell <elwell.christopher@gmail.com>
2019-09-06 13:39:59 +0000

    use more restrictive email address regex everywhere

--- src/cljs/app/vetd_app/common/pages/signup.cljs
+++ src/cljs/app/vetd_app/common/pages/signup.cljs
@@ -27,7 +27,7 @@
    (let [[bad-input message]
          (cond
            (not (re-matches #".+\s.+" uname)) [:uname "Please enter your full name (first & last)."]
-           (not (re-matches #"^\S+@\S+\.\S+$" email)) [:email "Please enter a valid email address."]
+           (not (util/valid-email-address? email)) [:email "Please enter a valid email address."]
            (< (count pwd) 8) [:pwd "Password must be at least 8 characters."]
            (not= pwd cpwd) [:cpwd "Password and Confirm Password must match."]
            (not terms-agree) [:terms-agree "You must agree to the Terms of Use in order to sign up."]
