9e1cf36cd17250dd2ae461a975a831911c91c2fa
Christopher Elwell <elwell.christopher@gmail.com>
2019-09-09 17:52:39 +0000

    hacky solution to small window widths causing display issues

--- src/clj/com/vetd/app/server.clj
+++ src/clj/com/vetd/app/server.clj
@@ -218,6 +218,7 @@
      [:div {:class "spinner"
             :style "margin: 175px auto;"}
       [:i] [:i] [:i]]]
+    [:div.window-size-message "Vetd is optimized for desktop use with a window width of 1200px or more."]
     ;; load Segment's "Analytics.js"
     [:script {:type "text/javascript"}
      "!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error(\"Segment snippet included twice.\");else{analytics.invoked=!0;analytics.methods=[\"trackSubmit\",\"trackClick\",\"trackLink\",\"trackForm\",\"pageview\",\"identify\",\"reset\",\"group\",\"track\",\"ready\",\"alias\",\"debug\",\"page\",\"once\",\"off\",\"on\"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement(\"script\");n.type=\"text/javascript\";n.async=!0;n.src=\"https://cdn.segment.com/analytics.js/v1/\"+t+\"/analytics.min.js\";var a=document.getElementsByTagName(\"script\")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION=\"4.1.0\";}}();"]
