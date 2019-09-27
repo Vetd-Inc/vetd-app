1366fb6db922e69a32c67013d5d3f05a9774bc98
Christopher Elwell <elwell.christopher@gmail.com>
2019-09-27 19:39:18 +0000

    vendor redirect upon toggle

--- src/cljs/app/vetd_app/core.cljs
+++ src/cljs/app/vetd_app/core.cljs
@@ -137,6 +137,11 @@
  (fn [{:keys [oname]}] oname))
 
 (rf/reg-sub
+ :vendor?
+ :<- [:active-org] 
+ (fn [{:keys [vendor?]}] vendor?))
+
+(rf/reg-sub
  :group-ids
  :<- [:active-org] 
  (fn [{:keys [groups]}]
